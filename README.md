b1228c4f41dbb416f307c36133da163b97b42d7c
Max Klein <isalix@gmail.com>
2020-10-05 16:17:14 -0700

    get an example running with scoped_session

---
+++ README.md
@@ -0,0 +1,4 @@
+## Requirements
+1. This is built from using pipenv, please see Pipfile.
+2. However this also requires [humaniki-schema](https://github.com/notconfusing/humaniki-schema), which in development mode, must be in a sibling directory to humaniki-backend.
+    2. you can have Pycharm autocomplete for you out of humaniki-schema by going to file->settings->project->structure, and adding a new content root.
--- humaniki-backend/app.py
+++ humaniki-backend/app.py
@@ -1,11 +1,20 @@
-from flask import Flask, jsonify
-from humaniki_schema.schema import metric
-app = Flask(__name__)
+from flask import Flask, abort, jsonify
+from humaniki_schema.schema import metric, metric_aggregations_j, metric_aggregations_n, metric_coverage, metric_properties_j, metric_properties_n, project, human_property, human, human_country, human_occupation, human_sitelink
+from humaniki_schema.utils import PopulationDefinition, FillType
 
-@app.route("/")
-def hello():
-    ametric = metric()
-    return jsonify(ametric.population_id)
+from humaniki_schema.db import session_factory
+from flask_sqlalchemy_session import flask_scoped_session
+
+app = Flask(__name__)
+session = flask_scoped_session(session_factory, app)
+
+@app.route("/metric/<int:fill_id>")
+def metric_example(fill_id):
+    ametric = session.query(metric).filter(metric.fill_id==fill_id).first()
+    if ametric is None:
+        abort(404)
+    return jsonify(**ametric.to_dict())
 
 if __name__ == "__main__":
     app.run()
+
