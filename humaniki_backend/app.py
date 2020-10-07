19cd908ab6a8919681e193a1f9a9420ec807294d
Max Klein <isalix@gmail.com>
2020-10-06 20:27:44 -0700

    the slow way to compile the metric query, still need to build the response

--- humaniki_backend/app.py
+++ humaniki_backend/app.py
@@ -3,7 +3,8 @@ from flask import Flask, abort, jsonify, request
 from humaniki_schema.db import session_factory
 from flask_sqlalchemy_session import flask_scoped_session
 
-from humaniki_backend.query import get_properties_id, get_metrics, get_latest_fill_id
+from humaniki_backend.query import get_properties_id, get_aggregations_ids, get_metrics, get_latest_fill_id, \
+    order_query_params
 from humaniki_backend.utils import determine_population_conflict, build_gap_response, assert_gap_request_valid
 
 app = Flask(__name__)
@@ -12,39 +13,38 @@ session = flask_scoped_session(session_factory, app)
 # Note this requires updating or the process restarting after a new fill.
 latest_fill_id, latest_fill_date = get_latest_fill_id(session)
 
+
 @app.route("/")
 def home():
     return jsonify(latest_fill_id, latest_fill_date)
 
 
-@app.route("/v1/gender/gap/<string:snapshot>/<string:population>/")
+@app.route("/v1/gender/gap/<string:snapshot>/<string:population>/properties")
 def gap(snapshot, population):
     return_warnings = {}
-    query_values = request.values
+    errors = {}
+    query_params = request.values
     try:
-        valid_request = assert_gap_request_valid(snapshot, population, query_values)
+        valid_request = assert_gap_request_valid(snapshot, population, query_params)
     except AssertionError as ae:
         return jsonify(ae)
-    #
-    population_correction = determine_population_conflict(population, query_values)
+    population_correction = determine_population_conflict(population, query_params)
     if population:
         return_warnings['population_correction'] = population_correction
-
+    ordered_query_params = order_query_params(query_params)
     try:
-        properties_id = get_properties_id(session, query_values)
+        properties_id = get_properties_id(session, ordered_query_params)
     except ValueError as ve:
-        raise
-        return jsonify(ve)
-
-    # TODO
-    # may need to get an aggregations_id here
-    aggregations_id = None
-
+        errors['properties_id'] = str(ve)
+    try:
+        aggregations_id = get_aggregations_ids(session, ordered_query_params)
+    except ValueError as ve:
+        errors['aggregations_id'] = str(ve)
     try:
         metrics = get_metrics(session, fill_id=latest_fill_id, population=population, properties_id=properties_id,
                               aggregations_id=aggregations_id)
     except ValueError as ve:
-        return jsonify(ve)
+        errors['aggregations_id'] = str(ve)
     response = build_gap_response(metrics)
     return jsonify(**response)
 
--- humaniki_backend/query.py
+++ humaniki_backend/query.py
@@ -5,31 +5,71 @@ from humaniki_schema.schema import metric, metric_aggregations_j, metric_aggrega
 
 from sqlalchemy import func
 
-def get_properties_id(session, query_values):
+
+def get_pid_from_str(property_str):
+    return getattr(Properties, property_str.upper()).value
+
+
+def order_query_params(query_params):
+    # first have to match the properties to their to numbers
+    pid_val = {get_pid_from_str(p_str): val for p_str, val in query_params.items()}
+    sorted_pids = sorted(pid_val.keys())
+    sorted_pid_val = {}
+    # In Python 3.7 dictionaries keep insertion order https://stackoverflow.com/a/40007169
+    for pid in sorted_pids:
+            sorted_pid_val[pid] = pid_val[pid]
+    return sorted_pid_val
+
+
+def get_properties_id(session, ordered_query_params):
     # get the properties ID based on the properties or return Error
-    query_keys = [k.upper() for k in query_values.keys()]
-    properties = [getattr(Properties, k) for k in query_keys if k in Properties.__members__]
-    properties_ints = [prop.value for prop in properties]
-    ordered_properties = sorted(properties_ints)
+    ordered_properties = ordered_query_params.keys()
     properties_id_q = session.query(metric_properties_j.id)
     for pos, prop_num in enumerate(ordered_properties):
-        properties_id_q = properties_id_q.filter(metric_properties_j.properties[pos]==prop_num)
+        print(pos, prop_num)
+        properties_id_q = properties_id_q.filter(metric_properties_j.properties[pos] == prop_num)
     print(f"Properties query {properties_id_q}")
+    # TODO see if using subqueries is faster
+    properties_id_subquery = properties_id_q.subquery()
     properties_id = properties_id_q.all()
     print(f"Properties id is: {properties_id}")
     return properties_id
 
 
+def get_aggregations_ids(session, ordered_query_params):
+    # aggregations_id is None indicates there's no contraint on the aggregation_id
+    ordered_aggregations = ordered_query_params.values()
+    if all([v == 'all' for v in ordered_aggregations]):
+        return None
+    else:
+        aggregations_id_q = session.query(metric_aggregations_j.id)
+        for pos, agg_val in enumerate(ordered_aggregations):
+            if agg_val != 'all': #hope there is no value called all
+                aggregations_id_q = aggregations_id_q.filter(metric_aggregations_j.properties[pos] == agg_val)
+        print(f"aggregations query {aggregations_id_q}")
+        # TODO see if using subqueries is faster
+        aggregations_id_subquery = aggregations_id_q.subquery()
+        aggregations_id = aggregations_id_q.all()
+        print(f"aggregations_id is: {aggregations_id}")
+        return aggregations_id
+
+
 def get_metrics(session, fill_id, population, properties_id, aggregations_id, use_lang='en'):
-    # get the properties ID based on the properties or return Error
-    properties_id = None
-    return properties_id
+    # get the properties ID based on the properties or return Error.
+    metrics_q = session.query(metric, metric_properties_j, metric_aggregations_j)\
+                .filter(metric_properties_j.id==properties_id)
+    if isinstance(aggregations_id, int):
+        metrics_q = metrics_q.filter(metric_aggregations_j.id==aggregations_id)
+    elif isinstance(aggregations_id, list):
+        metrics_q = metrics_q.filter(metric_aggregations_j.id.in_(aggregations_id))
+    metrics = metrics_q.all()
+    return metrics
 
 
 def get_latest_fill_id(session):
     latest_q = session.query(func.max(fill.date)).subquery()
 
-    q = session.query(fill.id, fill.date).filter(fill.date==latest_q)
+    q = session.query(fill.id, fill.date).filter(fill.date == latest_q)
 
     latest_fill_id, latest_fill_date = q.one()
     return latest_fill_id, latest_fill_date
